bfde41f0d0464ea779d9cd4b6244c410c66cd84e
pandaxcore <pandadeisgn7@gmail.com>
2021-05-23 13:41:09 +0600

    register page bind with CRUD database

--- comments-backend/app/Http/Controllers/UserController.php
+++ comments-backend/app/Http/Controllers/UserController.php
@@ -4,10 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
 class UserController extends Controller
 {
     //
-    function register(){
-        return "this is an Register function";
+    function register(Request $req){
+
+        // return "this is an Register function";
+
+        return $req -> input();
+        $user = new User;
+        $user -> name = $req -> input('name');
+        $user -> email = $req -> input('email');
+        $user -> password = $req -> input('password');
+        // $user -> password = Hash::make($req -> input('password'));
+        $user -> save();
+
+        return $user;
     }
 }
